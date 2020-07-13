66a833171076bc972b172c53a99ce46e1d632933
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-07-13 01:44:01 -0400

    auto find features

--- AutoRandomTree.py
+++ AutoRandomTree.py
@@ -16,41 +16,67 @@ data_file_path = "./data/train.csv"
 data = pd.read_csv(data_file_path)
 dataFrame = pd.DataFrame(data)
 
-# target object
-target_data = data["SalePrice"]
-
-raw_features = []
-features = []
-
 meta_features = pd.read_csv(features_file_path)
+meta_features_dataframe = pd.DataFrame(meta_features)
 
-for index, meta_feature in pd.DataFrame(meta_features).iterrows():
-    if meta_feature[1] == 1:
-        raw_features.append(meta_feature["feature_name"])
+def find_mae():
+    # target object
+    target_data = data["SalePrice"]
 
-# transform category data
-encoder = LabelEncoder()
+    raw_features = []
+    features = []
 
-for feature in raw_features:
-    # print("Column: " + feature + " | " + "Data type: " + str(dataFrame[feature].dtypes))
-    if dataFrame[feature].dtypes is not int:
-        # category data
-        new_feature_name = feature + "_encoded"
-        data[new_feature_name] = encoder.fit_transform(data[feature].astype(str))
-        features.append(new_feature_name)
+    for meta_feature in meta_features_dataframe.iterrows():
+        if meta_feature[1]['is_include'] == 1:
+            raw_features.append(meta_feature[1]["feature_name"])
 
-train_data = data[features]
+    # transform category data
+    encoder = LabelEncoder()
 
-# split train_data to sub training data and validation data
-sub_training_data, sub_value, validation_data, validation_value = train_test_split(
-    train_data, target_data, random_state=1
-)
+    for feature in raw_features:
+        # print("Column: " + feature + " | " + "Data type: " + str(dataFrame[feature].dtypes))
+        if dataFrame[feature].dtypes is not int:
+            # category data
+            new_feature_name = feature + "_encoded"
+            data[new_feature_name] = encoder.fit_transform(data[feature].astype(str))
+            features.append(new_feature_name)
 
-# specify model
-model = RandomForestRegressor(random_state=1)
-model.fit(sub_training_data, validation_data)
-predictions = model.predict(sub_value)
+    train_data = data[features]
 
-mea = mean_absolute_error(predictions, validation_value)
+    # split train_data to sub training data and validation data
+    sub_training_data, sub_value, validation_data, validation_value = train_test_split(
+        train_data, target_data, random_state=1
+    )
 
-print("Validation MAE for Random Forest Model: {:,.0f}".format(mea))
+    # specify model
+    model = RandomForestRegressor(random_state=1)
+    model.fit(sub_training_data, validation_data)
+    predictions = model.predict(sub_value)
+
+    mae = mean_absolute_error(predictions, validation_value)
+
+    print("Validation MAE for Random Forest Model: {:,.0f}".format(mae))
+
+    return mae
+
+def find_features():
+    mae = -1
+
+    for meta_feature in meta_features_dataframe.iterrows():
+        if meta_feature[1]['is_checked'] == 0:
+            # do the work
+            meta_feature[1]['is_include'] = 1
+            new_mae = find_mae()
+            if mae == -1:
+                mae = new_mae
+            elif new_mae <= mae:
+                mae = new_mae
+            else:
+                meta_feature[1] = 0
+            
+            meta_feature[1]['is_checked'] = 1
+            meta_features_dataframe.to_csv(features_file_path)
+
+        print("LOCKED MAE: {:,.0f}".format(mae))
+
+find_features()
